76b819df81c1338dc806c5f5223ff645d352fa54
peachey <>
2005-07-21 17:40:59 +0000

    Test prefix() method and prefix stream manipulator for writing prefix at beginning of lines.

--- src/test/test_st_stream.cxx
+++ src/test/test_st_stream.cxx
@@ -28,9 +28,9 @@ void sample1() {
   // Optional: set name of current function.
   formatter1().setMethod("sample1()");
 
-  formatter1().info() << "Wrote to formatter1().info() (with implicit default chatter of 3)" << std::endl;
-  formatter1().info(1) << "Wrote to formatter1().info(1)" << std::endl;
-  formatter1().info(4) << "Wrote to formatter1().info(4). THIS SHOULD NOT APPEAR because the chatter is " <<
+  formatter1().info().prefix() << "Wrote to formatter1().info() (with implicit default chatter of 3)" << std::endl;
+  formatter1().info(1).prefix() << "Wrote to formatter1().info(1)" << std::endl;
+  formatter1().info(4).prefix() << "Wrote to formatter1().info(4). THIS SHOULD NOT APPEAR because the chatter is " <<
     "higher than the global maximum chatter level." << std::endl;
 }
 
@@ -40,10 +40,10 @@ void sample2() {
 
   // A formatter specific to this function.
   StreamFormatter formatter2("", "sample2()", 4);
-  formatter2.warn() << "Wrote to formatter2.warn(). THIS SHOULD NOT APPEAR because formatter2's " <<
+  formatter2.warn() << prefix << "Wrote to formatter2.warn(). THIS SHOULD NOT APPEAR because formatter2's " <<
     "default chatter is higher than the global maximum chatter level." << std::endl;
-  formatter2.warn(3) << "Wrote to formatter2.warn(3). This should appear. " <<
-    "Note that the prefix repeats at the" << std::endl << "beginning of each new line." << std::endl;
+  formatter2.warn(3) << prefix << "Wrote to formatter2.warn(3). This should appear. " <<
+    "Note that the prefix repeats at the" << std::endl << prefix << "beginning of each new line." << std::endl;
 }
 
 void sample3() {
@@ -57,21 +57,22 @@ void sample3() {
 
       void myDebug() {
         m_formatter3.setMethod("myDebug");
-        m_formatter3.debug() << "Wrote to m_formatter3.debug(). If debugging were disabled, this would not appear." << std::endl;
+        m_formatter3.debug() << prefix << "Wrote to m_formatter3.debug(). If debugging were disabled, this would not appear." <<
+          std::endl;
 
         // Get current debugging state.
         bool debug_mode = GetDebugMode();
 
         // Explicitly enable debugging. Useful, but only during development.
         m_formatter3.setDebugMode();
-        m_formatter3.debug() << "Wrote to m_formatter3.debug(). This would appear even if debugging were initially disabled." <<
-          std::endl;
+        m_formatter3.debug().prefix() <<
+          "Wrote to m_formatter3.debug(). This would appear even if debugging were initially disabled." << std::endl;
 
         // Explicitly disable debugging. Useful, but only during development.
         m_formatter3.setDebugMode(false);
-        m_formatter3.debug() << "Wrote to m_formatter3.debug(). THIS SHOULD NOT APPEAR because debugging was locally disabled." <<
-          std::endl;
-        m_formatter3.debug() << "This would not affect the debug setting of any other objects." << std::endl;
+        m_formatter3.debug() << prefix <<
+          "Wrote to m_formatter3.debug(). THIS SHOULD NOT APPEAR because debugging was locally disabled." << std::endl;
+        m_formatter3.debug() << prefix << "This would not affect the debug setting of any other objects." << std::endl;
 
         // Restore debugging state.
         m_formatter3.setDebugMode(debug_mode);
@@ -79,22 +80,22 @@ void sample3() {
 
       void myErr() {
         m_formatter3.setMethod("myErr");
-        m_formatter3.err() << "Wrote to m_formatter3.err()" << std::endl;
+        m_formatter3.err() << prefix << "Wrote to m_formatter3.err()" << std::endl;
       }
 
       void myInfo() {
         m_formatter3.setMethod("myInfo");
-        m_formatter3.info() << "Wrote to m_formatter3.info()" << std::endl;
+        m_formatter3.info() << prefix << "Wrote to m_formatter3.info()" << std::endl;
       }
 
       void myOut() {
         m_formatter3.setMethod("myOut");
-        m_formatter3.out() << "Wrote to m_formatter3.out()" << std::endl;
+        m_formatter3.out() << prefix << "Wrote to m_formatter3.out()" << std::endl;
       }
 
       void myWarn() {
         m_formatter3.setMethod("myWarn");
-        m_formatter3.warn() << "Wrote to m_formatter3.warn()" << std::endl;
+        m_formatter3.warn() << prefix << "Wrote to m_formatter3.warn()" << std::endl;
       }
 
     private:
@@ -150,20 +151,20 @@ void sample4() {
         m_os.setMethod("B()");
 
         // Unsuppressible output.
-        m_os.out() << "The statistic was " << m_stat << endl;
+        m_os.out() << prefix << "The statistic was " << m_stat << endl;
 
         // Unsuppressible error.
-        m_os.err() << "The statistic was " << m_stat << endl;
+        m_os.err() << prefix << "The statistic was " << m_stat << endl;
 
         // Suppressible information.
         // Note that this will not display anything if the chatter level is too low (although for this test program the
         // chatter level is fixed at 3). Note also that the name of class and method is only displayed because st_stream
         // is in debug mode.
-        m_os.info() << "The statistic was " << m_stat << endl;
+        m_os.info() << prefix << "The statistic was " << m_stat << endl;
 
         // Suppressible warning.
         // Notes above the use of info() also apply to warn().
-        m_os.warn() << "The statistic was " << m_stat << endl;
+        m_os.warn() << prefix << "The statistic was " << m_stat << endl;
       }
 
     private:
@@ -180,7 +181,7 @@ void sample4() {
 
 int main() {
   // Before initializing standard streams, write to stout. This should have no effect.
-  stout << "This was written before initializing standard streams, so THIS SHOULD NOT APPEAR on any stream!" << std::endl;
+  stout << prefix << "This was written before initializing standard streams, so THIS SHOULD NOT APPEAR on any stream!" << std::endl;
 
   // Set max_chat used for tests.
   unsigned int max_chat = 3;
@@ -202,9 +203,9 @@ int main() {
   std::cout << "After this, there should be exactly one line on each of sterr, stlog and stout." << std::endl;
 
   // Test direct writes to standard streams.
-  sterr << "This should appear on std::cerr." << std::endl;
-  stlog << "This should appear on std::clog." << std::endl;
-  stout << "This should appear on std::cout." << std::endl;
+  sterr << prefix << "This should appear on std::cerr." << std::endl;
+  stlog << prefix << "This should appear on std::clog." << std::endl;
+  stout << prefix << "This should appear on std::cout." << std::endl;
 
   // Create a file-based STL stream.
   std::string out_file = "test_st_stream-out";
@@ -228,9 +229,9 @@ int main() {
 
   // Write something directly to each stream.
   std_os << "Three lines should now be written to this file with no prefix, one via each stream:" << std::endl;
-  sterr << Chat(max_chat + 1) << "Despite Chat(" << max_chat + 1 << ") this was written to sterr." << std::endl;
-  stlog << Chat(max_chat + 1) << "Despite Chat(" << max_chat + 1 << ") this was written to stlog." << std::endl;
-  stout << Chat(max_chat + 1) << "Despite Chat(" << max_chat + 1 << ") this was written to stout." << std::endl;
+  sterr << Chat(max_chat + 1) << prefix << "Despite Chat(" << max_chat + 1 << ") this was written to sterr." << std::endl;
+  stlog << Chat(max_chat + 1) << prefix << "Despite Chat(" << max_chat + 1 << ") this was written to stlog." << std::endl;
+  stout << Chat(max_chat + 1) << prefix << "Despite Chat(" << max_chat + 1 << ") this was written to stout." << std::endl;
 
   // Create a stream which forwards to stout, but uses chatter, which can be used to suppress some output.
   OStream my_out(true);
@@ -238,12 +239,13 @@ int main() {
 
   // Test that this works correctly with different chat levels.
   std_os << "Three lines should now be written to this file via my_out:" << std::endl;
-  my_out << "This was written to my_out with default chat level." << std::endl;
-  my_out << Chat(max_chat - 1) << "This was written to my_out with maximum chatter set to " << max_chat - 1 << std::endl;
-  my_out << Chat(max_chat) << "This was written to my_out with maximum chatter set to " << max_chat << std::endl;
-  my_out << Chat(max_chat + 1) <<
+  my_out << prefix << "This was written to my_out with default chat level." << std::endl;
+  my_out << Chat(max_chat - 1) << prefix << "This was written to my_out with maximum chatter set to " << max_chat - 1 << std::endl;
+  my_out << Chat(max_chat) << prefix << "This was written to my_out with maximum chatter set to " << max_chat << std::endl;
+  my_out << Chat(max_chat + 1) << prefix <<
     "THIS SHOULD NOT APPEAR! This was written to my_out with maximum chatter set to " << max_chat + 1 << std::endl;
-  my_out << "THIS SHOULD NOT APPEAR! This was written to my_out after maximum chatter was set to " << max_chat + 1 << std::endl;
+  my_out << prefix << "THIS SHOULD NOT APPEAR! This was written to my_out after maximum chatter was set to " <<
+    max_chat + 1 << std::endl;
 
   // Add the method name to the output stream's prefix.
   my_out.setPrefix("exec_name: ");
@@ -252,51 +254,54 @@ int main() {
   std_os << "The next line should begin with a prefix of \"exec_name: \"" << std::endl;
 
   // Write line with prefix.
-  my_out << Chat(max_chat) <<
+  my_out << Chat(max_chat) << prefix <<
     "This was written to my_out with explicit Chat(" << max_chat << ") after prefix was set." << std::endl;
 
   // Create a formatter with a default chatter level which should not be displayed.
   StreamFormatter sf1("", "", max_chat + 1);
   std_os << "A line with a prefix of \"test_st_stream: WARNING: \" should follow this line." << std::endl;
-  sf1.warn() << "THIS SHOULD NOT APPEAR! This was written to sf1.warn()" << std::endl;
-  sf1.warn(0) << "This was written to sf1.warn(0), and should always appear." << std::endl;
-  sf1.warn() << "THIS SHOULD NOT APPEAR! This was written to sf1.warn(" << max_chat + 1 << ")." << std::endl;
+  sf1.warn() << prefix << "THIS SHOULD NOT APPEAR! This was written to sf1.warn()" << std::endl;
+  sf1.warn(0) << prefix << "This was written to sf1.warn(0), and should always appear." << std::endl;
+  sf1.warn() << prefix << "THIS SHOULD NOT APPEAR! This was written to sf1.warn(" << max_chat + 1 << ")." << std::endl;
 
   // Now include a method name in the warning.
   std_os << "A line with a prefix of \"test_st_stream: WARNING: main(): \" should follow this line." << std::endl;
   sf1.setMethod("main()");
-  sf1.warn(max_chat) << "This was written to sf1.warn() after setMethod(...)" << std::endl;
+  sf1.warn(max_chat) << prefix << "This was written to sf1.warn() after setMethod(...)" << std::endl;
 
   // Create a formatter with a single prefix.
   StreamFormatter sf2("AClassName", "", max_chat);
   std_os << "Two lines with a prefix of \"test_st_stream: WARNING: AClassName: \" should follow this line." << std::endl;
-  sf2.warn(max_chat + 1) << "THIS SHOULD NOT APPEAR! This was written to sf2.warn(" << max_chat + 1 << ")." << std::endl;
-  sf2.warn(0) << "This was written to sf2.warn(0), and should always appear." << std::endl;
-  sf2.warn() << "This was written to sf2.warn() with a default chatter of " << max_chat << std::endl;
+  sf2.warn(max_chat + 1) << prefix << "THIS SHOULD NOT APPEAR! This was written to sf2.warn(" << max_chat + 1 << ")." << std::endl;
+  sf2.warn(0) << prefix << "This was written to sf2.warn(0), and should always appear." << std::endl;
+  sf2.warn() << prefix << "This was written to sf2.warn() with a default chatter of " << max_chat << std::endl;
 
   // Now include a method name in the warning.
   std_os << "A line with a prefix of \"test_st_stream: WARNING: AClassName::aMethod(int): \" should follow this line." <<
     std::endl;
   sf2.setMethod("aMethod(int)");
-  sf2.warn() << "This was written to sf2.warn() after setMethod(...)" << std::endl;
+  sf2.warn() << prefix << "This was written to sf2.warn() after setMethod(...)" << std::endl;
 
   // Test debug statements, which should be issued no matter what if debugging is enabled.
-  std_os << "A line with prefix \"test_st_stream: DEBUG: AClassName::aMethod(int): \" should follow this line." << std::endl;
-  sf2.debug() << Chat(std::numeric_limits<unsigned int>::max()) << "This was written to sf2.debug() with highest possible chat." <<
+  std_os << "A line with prefix \"test_st_stream: DEBUG: AClassName::aMethod(int): \" should follow this line." <<
     std::endl;
+  sf2.debug() << Chat(std::numeric_limits<unsigned int>::max()) << prefix <<
+    "This was written to sf2.debug() with highest possible chat." << std::endl;
 
   // Test info method with debugging enabled.
-  std_os << "A line with prefix \"test_st_stream: INFO: AClassName::aMethod(int): \" should follow this line." << std::endl;
-  sf2.info(0) << "This was written to sf2.info(0), and should always appear." << std::endl;
+  std_os << "A line with prefix \"test_st_stream: INFO: AClassName::aMethod(int): \" should follow this line." <<
+    std::endl;
+  sf2.info(0) << prefix << "This was written to sf2.info(0), and should always appear." << std::endl;
 
   // Test err method with debugging enabled.
-  std_os << "A line with prefix \"test_st_stream: ERROR: AClassName::aMethod(int): \" should follow this line." << std::endl;
-  sf2.err() << Chat(std::numeric_limits<unsigned int>::max()) <<
+  std_os << "A line with prefix \"test_st_stream: ERROR: AClassName::aMethod(int): \" should follow this line."
+    << std::endl;
+  sf2.err() << Chat(std::numeric_limits<unsigned int>::max()) << prefix <<
     "This was written to sf2.err(), and should always appear despite its highest possible chatter level." << std::endl;
 
   // Test out method with debugging enabled.
   std_os << "A line with prefix \"test_st_stream: \" should follow this line." << std::endl;
-  sf2.out() << Chat(std::numeric_limits<unsigned int>::max()) <<
+  sf2.out() << Chat(std::numeric_limits<unsigned int>::max()) << prefix <<
     "This was written to sf2.out(), and should always appear despite its highest possible chatter level." << std::endl;
 
   // Get current debugging state.
@@ -304,7 +309,7 @@ int main() {
 
   // Turn off debug statements. Verify that debug statements no longer appear no matter what.
   sf2.setDebugMode(false);
-  sf2.debug() << Chat(0) << "THIS SHOULD NOT APPEAR! This was written to sf2.debug() after sf2.setDebugMode(false)." <<
+  sf2.debug() << Chat(0) << prefix << "THIS SHOULD NOT APPEAR! This was written to sf2.debug() after sf2.setDebugMode(false)." <<
     std::endl;
 
   // Disable debugging.
@@ -312,16 +317,16 @@ int main() {
 
   // Test info method with debugging disabled.
   std_os << "A line with prefix \"test_st_stream: INFO: \" should follow this line." << std::endl;
-  sf2.info(0) << "This was written to sf2.info(0), and should always appear." << std::endl;
+  sf2.info(0) << prefix << "This was written to sf2.info(0), and should always appear." << std::endl;
 
   // Test err method with debugging disabled.
   std_os << "A line with prefix \"test_st_stream: ERROR: \" should follow this line." << std::endl;
-  sf2.err() << Chat(std::numeric_limits<unsigned int>::max()) <<
+  sf2.err() << Chat(std::numeric_limits<unsigned int>::max()) << prefix <<
     "This was written to sf2.err(), and should always appear despite its highest possible chatter level." << std::endl;
 
   // Test out method with debugging disabled.
   std_os << "A line with prefix \"test_st_stream: \" should follow this line." << std::endl;
-  sf2.out() << Chat(std::numeric_limits<unsigned int>::max()) <<
+  sf2.out() << Chat(std::numeric_limits<unsigned int>::max()) << prefix <<
     "This was written to sf2.out(), and should always appear despite its highest possible chatter level." << std::endl;
 
   // Restore debugging state.
@@ -330,19 +335,20 @@ int main() {
   // Test setting stream precision.
   double a_double = 1.23456789012;
 
-  stout << "With default precision, 1.23456789012 is displayed as " << a_double << std::endl;
+  stout << prefix << "With default precision, 1.23456789012 is displayed as " << a_double << std::endl;
   stout.precision(12);
-  stout << "After precision was set to 12, 1.23456789012 is displayed as " << a_double << std::endl;
+  stout << prefix << "After precision was set to 12, 1.23456789012 is displayed as " << a_double << std::endl;
 
   // Test setting format flags.
-  stout << "With default format flags, 16 is displayed as " << 16 << std::endl;
+  stout << prefix << "With default format flags, 16 is displayed as " << 16 << std::endl;
   stout.flags(std::ios_base::hex);
-  stout << "With format flags hex, 16 is displayed as " << 16 << std::endl;
+  stout << prefix << "With format flags hex, 16 is displayed as " << 16 << std::endl;
 
   // Test field width and fill characters.
-  stout << "With default width and fill character, 1.234 shifted twice gives " << 1.234 << 1.234 << std::endl;
+  stout << prefix << "With default width and fill character, 1.234 shifted twice gives " << 1.234 << 1.234 << std::endl;
   stout.setf(std::ios_base::left, std::ios_base::adjustfield);
   stout.fill('#');
+  stout.prefix();
   stout.width(16);
   stout << "With width == 16 and fill character #, 1.234 shifted twice gives ";
   stout.width(16);
