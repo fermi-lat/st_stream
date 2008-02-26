aae6be5ef5736e458e082010fec3e5a9bc2c2aed
glastrm <>
2008-02-26 16:23:28 +0000

    Add lib dependencies when creating a library

--- st_streamLib.py
+++ st_streamLib.py
@@ -1,6 +1,7 @@
-#$Id$
+#$Id: st_streamLib.py,v 1.2 2008/02/22 00:53:50 golpa Exp $
 def generate(env, **kw):
-	env.Tool('addLibrary', library = ['st_stream'], package = 'st_stream')
+	if not kw.get('depsOnly',0):
+		env.Tool('addLibrary', library = ['st_stream'])
 
 def exists(env):
 	return 1
