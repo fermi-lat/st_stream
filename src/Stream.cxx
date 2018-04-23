0246850adfe5eb6780a902d1aed9db511e77b6f0
jasercion <jasercion.gwu@gmail.com>
2018-04-23 15:30:21 -0400

    change basic_ios ref to ios_base

--- src/Stream.cxx
+++ src/Stream.cxx
@@ -2,7 +2,7 @@
     \brief Implementation of OStream class.
     \author James Peachey, HEASARC/GSSC
 */
-#include <ostream>
+//#include <ostream>
 #include "st_stream/Stream.h"
 #include "st_stream/st_stream.h"
 
@@ -106,11 +106,11 @@ namespace st_stream {
   }
 
   char OStream::fill() const {
-    return getStreamState<char, std::basic_ios<char> >(&std::ostream::fill, &OStream::fill);
+    return getStreamState<char, std::ios_base<char> >(&std::ostream::fill, &OStream::fill);
   }
 
   char OStream::fill(char new_fill) {
-    return setStreamState<char, std::basic_ios<char> >(&std::ostream::fill, &OStream::fill, &OStream::fill, new_fill);
+    return setStreamState<char, std::ios_base<char> >(&std::ostream::fill, &OStream::fill, &OStream::fill, new_fill);
   }
 
   OStream & prefix(OStream & os) { return os.prefix(); }
