0bc6023624a863ded032e639203117134e3dd75a
jasercion <jasercion.gwu@gmail.com>
2018-04-23 17:17:33 -0400

    Update Stream.cxx

--- src/Stream.cxx
+++ src/Stream.cxx
@@ -106,11 +106,11 @@ namespace st_stream {
   }
 
   char OStream::fill() const {
-    return getStreamState<char, ios::basic_ios<char> >(&std::ostream::fill, &OStream::fill);
+    return getStreamState<char, std::ios::basic_ios<char> >(&std::ostream::fill, &OStream::fill);
   }
 
   char OStream::fill(char new_fill) {
-    return setStreamState<char, ios::basic_ios<char> >(&std::ostream::fill, &OStream::fill, &OStream::fill, new_fill);
+    return setStreamState<char, std::ios::basic_ios<char> >(&std::ostream::fill, &OStream::fill, &OStream::fill, new_fill);
   }
 
   OStream & prefix(OStream & os) { return os.prefix(); }
